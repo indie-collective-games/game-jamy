96a96318f483ffd9fb8d7df5e77f963bafc1fcc8
Christopher Humphries <engleek@gmail.com>
2018-01-28 16:28:54 +0100

    GO GO GO

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
-console.log('Mais dis-donc Jamy!');
+const button = document.querySelector('button');
+const music = document.querySelector('#music');
+const disdonc = document.querySelector('#disdonc');
+
+window.addEventListener('load', () => {
+  music.play();
+});
+
+const doTheThing = () => {
+  fetch('http://fr.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&format=json&origin=*')
+    .then(res => res.json())
+    .then(data => {
+      const { title, extract } = Object.values(data.query.pages)[0];
+      const titleSpeech = new SpeechSynthesisUtterance(`Dis-donc Jamy, c'est quoi ${title}?`);
+            titleSpeech.lang = 'fr-fr';
+      
+            music.pause()
+      window.speechSynthesis.speak(titleSpeech);
+    });
+}
+
+button.addEventListener('click', doTheThing, false);
