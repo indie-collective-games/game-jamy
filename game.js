75fc1ea46d6459d7eccb2f0aca352b0256020dcc
Christopher Humphries <engleek@gmail.com>
2018-01-28 16:42:35 +0100

    GO GO GO GO

--- game.js
+++ game.js
@@ -1,9 +1,16 @@
 const button = document.querySelector('button');
 const music = document.querySelector('#music');
 const disdonc = document.querySelector('#disdonc');
+const pageTitle = document.querySelector('#pagetitle');
+const pageSummary = document.querySelector('#pagesummary');
+const tressimple = document.querySelector('#showsummary');
 
 window.addEventListener('load', () => {
   music.play();
+});
+
+tressimple.addEventListener('click', () => {
+  pageSummary.style.display = 'block';
 });
 
 const doTheThing = () => {
@@ -11,11 +18,19 @@ const doTheThing = () => {
     .then(res => res.json())
     .then(data => {
       const { title, extract } = Object.values(data.query.pages)[0];
-      const titleSpeech = new SpeechSynthesisUtterance(`Dis-donc Jamy, c'est quoi ${title}?`);
+      const titleSpeech = new SpeechSynthesisUtterance(`C'est quoi ${title}?`);
             titleSpeech.lang = 'fr-fr';
       
-            music.pause()
-      window.speechSynthesis.speak(titleSpeech);
+      pageSummary.style.display = 'none';
+      
+      disdonc.addEventListener('ended', () => {
+        window.speechSynthesis.speak(titleSpeech);
+        pagetitle.innerText = title;
+        pageSummary.innerHTML = extract;
+      });
+
+      music.pause();
+      disdonc.play();
     });
 }
 
